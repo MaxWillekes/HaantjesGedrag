9ed4c49ad1b418d244c76da648bd2a472d2a109d
MaxWillekes <max.willekes@student.hku.nl>
2019-03-26 12:20:34 +0100

    Game scene

--- Assets/Scripts/characterScript.cs
+++ Assets/Scripts/characterScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class characterScript : MonoBehaviour
@@ -19,17 +20,19 @@ public class characterScript : MonoBehaviour
     public Animator animation;
     public Sprite idleSprite;
 
-    public Fungus.VariableReference CloseEnough;
     bool Close;
-    public Fungus.VariableReference Interact;
-    public Fungus.VariableReference InteractableCharacter;
-    public Fungus.VariableReference RecentlySpoken;
+
+    public Fungus.Flowchart Flowchart;
+
+    public bool scalingException = false;
 
     void Start()
     {
         Close = false;
         rb = GetComponent<Rigidbody2D>();
         ownSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        Flowchart = GameObject.Find("GameHandler").GetComponent<Fungus.Flowchart>();
     }
 
     public void Update()
@@ -38,42 +41,59 @@ public class characterScript : MonoBehaviour
         float inputVer = Input.GetAxisRaw("Vertical");
 
         //Debug.Log("Hor =" + inputHor + "   |   Ver =" + inputVer);
-        var actualMoveSpeed = ((calculationSpeed / (transform.position.y / 1.5f)) + moveSpeed);
+        if (scalingException == true)
+        {
+            rb.velocity = new Vector2(inputHor * moveSpeed, inputVer * moveSpeed);
+        }else{
+            var actualMoveSpeed = ((calculationSpeed / (transform.position.y / 1.5f)) + moveSpeed);
 
-        //Scales the player speed to the Y position of thier character
-        rb.velocity = new Vector2(inputHor * actualMoveSpeed, inputVer * actualMoveSpeed);
+            //Scales the player speed to the Y position of thier character
+            rb.velocity = new Vector2(inputHor * actualMoveSpeed, inputVer * actualMoveSpeed);
 
-        if(inputHor > 0 || inputVer > 0 || inputHor < 0 || inputVer < 0){
+            //Scales the player size according to the Y position of thier character.
+            transform.localScale = new Vector3(0.165f - (transform.position.y / 112), 0.165f - (transform.position.y / 112), 0.165f - (transform.position.y / 112));
+        }
+
+
+        if (inputHor > 0 || inputVer > 0 || inputHor < 0 || inputVer < 0)
+        {
             animation.enabled = true;
         }else{
             animation.enabled = false;
             ownSpriteRenderer.sprite = idleSprite;
         }
 
-        if(inputHor < 0){
+        if (inputHor < 0){
             ownSpriteRenderer.flipX = true;
-        }else if(inputHor > 0){
+        }else if (inputHor > 0){
             ownSpriteRenderer.flipX = false;
         }
 
-        //Scales the player size according to the Y position of thier character.
-        transform.localScale = new Vector3(0.165f - (transform.position.y / 112), 0.165f - (transform.position.y / 112), 0.165f - (transform.position.y / 112));
+        if (Input.GetKeyDown("space") && Close == true)
+        {
+            Flowchart.SetBooleanVariable("Interact", true);
+        }
 
-        if (Input.GetKeyDown("space") && Close == true ){
-            Interact.Set(true);
+        if(SceneManager.GetActiveScene().name == "Bar")
+        {
+            Flowchart.SetBooleanVariable("InBar", true);
         }
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         Interactable = collider.gameObject;
+        if(Interactable.name == "en_street_bd_bar")
+        {
+            SceneManager.LoadScene("Bar");
+        }else{
+            Flowchart.SetGameObjectVariable("InteractableCharacter", Interactable);
+            Flowchart.SetBooleanVariable("CloseEnough", true);
+            Close = true;
 
-        InteractableCharacter.Set(Interactable);
-        CloseEnough.Set(true);
-        Close = true;
-
-        var emission = Interactable.GetComponent<ParticleSystem>().emission;
-        emission.enabled = true;
+            var emission = Interactable.GetComponent<ParticleSystem>().emission;
+            emission.enabled = true;
+        }
     }
 
     void OnTriggerExit2D(Collider2D collider)
@@ -82,10 +102,10 @@ public class characterScript : MonoBehaviour
         emission.enabled = false;
 
         Interactable = null;
-        InteractableCharacter.Set(Interactable);
-        CloseEnough.Set(false);
-        Interact.Set(false);
+        Flowchart.SetGameObjectVariable("InteractableCharacter", Interactable);
+        Flowchart.SetBooleanVariable("CloseEnough", false);
+        Flowchart.SetBooleanVariable("Interact", false);
         Close = false;
-        RecentlySpoken.Set(false);
+        Flowchart.SetBooleanVariable("RecentlySpoken", false);
     }
 }
